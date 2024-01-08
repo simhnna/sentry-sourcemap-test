1fa389be3733e1adcb639bdb2dfd7888790fd3d2
Simon Hanna <simon.hanna@aeb.com>
2024-01-08 11:28:56 +0100

    Add dist

---
+++ dist/src/app/code.js
@@ -0,0 +1,11 @@
+"use strict";
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="8a228438-d680-5dc2-bb5e-c1958699ee85")}catch(e){}}();
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fail = void 0;
+function fail() {
+    throw new Error('bar');
+}
+exports.fail = fail;
+//# sourceMappingURL=code.js.map
+//# debugId=8a228438-d680-5dc2-bb5e-c1958699ee85
---
+++ dist/src/main.js
@@ -0,0 +1,36 @@
+"use strict";
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="8c33f459-4125-5f99-b0eb-9e8e274e8abd")}catch(e){}}();
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const code_1 = require("./app/code");
+const Sentry = __importStar(require("@sentry/node"));
+Sentry.init({
+    dsn: 'https://5d74aa453a97267c9facae9e38c7ae64@o4506535120797696.ingest.sentry.io/4506535122239488',
+    attachStacktrace: true,
+});
+(0, code_1.fail)();
+//# sourceMappingURL=main.js.map
+//# debugId=8c33f459-4125-5f99-b0eb-9e8e274e8abd
